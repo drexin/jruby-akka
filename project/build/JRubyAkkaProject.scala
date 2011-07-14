d50b5d3906f2a48d3b43dc4f8ea792314a34cb31
drexin <dario.rexin@r3-tech.de>
2011-07-15 01:18:15 +0200

    fixed project file

--- project/build/JRubyAkkaProject.scala
+++ project/build/JRubyAkkaProject.scala
@@ -6,22 +6,27 @@ class JRubyAkkaProject(info: ProjectInfo) extends DefaultProject(info) with Ecli
   val jruby = "org.jruby" % "jruby" % "1.6.3"
   val akkaActors = akkaModule("actor")
   val akkaStm = akkaModule("stm")
-  
+
   override def defaultJarName = "jruby-akka.jar"
   override def dependencyPath = "dependencies" / "lib"
   override def managedDependencyPath = "dependencies" / "lib_managed"
-  
-  override lazy val `update` = { 
-    val x = updateAction
-    val filesToCopy = Array(new File("project/boot/scala-"+buildScalaVersion+"/lib/scala-library.jar")) ++ 
-    		((new File((managedDependencyPath / "compile").toString)).listFiles ++
-    		(new File((dependencyPath).toString)).listFiles)
+
+  override lazy val `update` = updateAction
+  override def updateAction = task {
+    val x = super.updateAction.run
+    FileUtilities.createDirectory("javalib", log)
+    FileUtilities.createDirectory(dependencyPath, log)
+    val filesToCopy = Array(new File("project/boot/scala-"+buildScalaVersion+"/lib/scala-library.jar")) ++
+            ((new File((managedDependencyPath / "compile").toString)).listFiles ++
+            (new File((dependencyPath).toString)).listFiles)
     FileUtilities.copyFilesFlat(filesToCopy, "javalib", log)
     x
   }
-  
-  override lazy val `package` = {
-    val x = packageAction
+
+  override lazy val `package` = packageAction
+  override def packageAction = task {
+    val x = super.packageAction.run
+    FileUtilities.createDirectory("lib", log)
     FileUtilities.copyFile("target" / ("scala_"+buildScalaVersion) / defaultJarName, "lib" / defaultJarName, log)
     x
   }
